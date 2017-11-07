1dbfb24ec6850ca49fcd6abe1f5ca09ca091a2b1
comcont <immortalbeing10@gmail.com>
2017-11-06 23:11:05 -0500

    Setup babel and node

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../src/index.html'))
+});
+
+app.listen(port, function(err) {
+	if (err) throw err;
+	else open('http://localhost:' + port);
+})
